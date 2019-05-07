869a37eadff1532f66f3f3acaeb75c68698d4e11
Antonio <antoniozurlo89@gmail.com>
2019-05-07 18:49:04 +0200

    Metodo READ

--- Progetto ASD/src/ReteAutomi/ReteAutomi.java
+++ Progetto ASD/src/ReteAutomi/ReteAutomi.java
@@ -1,6 +1,8 @@
 package ReteAutomi;
 import org.jdom.*;
 import org.jdom.input.*;
+
+import java.util.Iterator;
 import java.util.List;
 import java.io.File;
 import java.util.ArrayList;
@@ -56,8 +58,44 @@ public class ReteAutomi {
             Document document = builder.build(new File(file));
             Element root = document.getRootElement();
             ReteAutomi rete =new ReteAutomi();
-            rete.setNome(root.getChild("name").getText());
-            List children = root.getChildren("Automa");
+            rete.setNome(root.getChild("Nome").getText());
+
+            Element automi =root.getChild("Automi");
+            List listAutomi = automi.getChildren("Automa");
+
+            for (int i = 0; i < listAutomi.size(); i++)
+            {
+                Automa automa = new Automa();
+                Element nodoAutoma = (Element) listAutomi.get(i);
+                automa.setNome(nodoAutoma.getChildText("Nome"));
+                Element stati =nodoAutoma.getChild("Stati");
+                List listStati = stati.getChildren("Stato");
+                for (int j = 0; j < listStati.size(); j++) {
+                    StatoSemplice stato = new StatoSemplice();
+                    Element nodoStato = (Element) listStati.get(i);
+                    stato.setId(nodoStato.getChildText("ID"));
+                    if (nodoStato.getChildText("StatoIniziale").compareTo("true")==0) {
+                       stato.setIniziale(true);
+                    }
+                    else{
+                        stato.setIniziale(false);
+                    }
+
+
+
+
+
+                }
+
+
+
+
+
+
+
+            }
+
+
 
         } catch (Exception e) {
             System.err.println("Errore durante la lettura dal file");
