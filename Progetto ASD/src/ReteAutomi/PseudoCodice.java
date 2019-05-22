8be1339e6e8e446c8dc4810b8e1c37a356170d96
Antonio <antoniozurlo89@gmail.com>
2019-05-22 10:02:54 +0200

    Aggiunta una classe di PseudoCodice da mettere nell'etichettatura.
    Fammi sapere cosa ne pensi.

---
+++ Progetto ASD/src/ReteAutomi/PseudoCodice.java
@@ -0,0 +1,23 @@
+package ReteAutomi;
+
+public class PseudoCodice {
+    /*
+    Else if (confermato==1)
+        {
+            ArrayList<String> rilevanzaFinale = new ArrayList ();
+            finale= t.getIniziale.getRilevanza();
+            finale.pushRilevanza(t.getRilevanza());
+            if !(finale.notEqual(successivo.getRilevanza())
+                {
+                    successivo.clone ();
+                    successivo.setRilevanza (finale);
+                    PIEDE RICORSIONE (etichettaturaRilevanza(successivo)) ????????
+                }
+            else if (finale.equal(successivo.getRilevanza())
+            {
+                DOBBIAMO DAVVERO VALUTARE QUESTO CASO? ALLA FINE SE LO STATO è GIà STATO VISITATO CON LE STESSE ETICHETTE DI RILEVANZA CI POSSIAMO FERMARE NO?
+                LA TRANSIZIONE CHE PORTA A QUELLO STATO E QUELLO STATO STESSO GIà ESISTONO... MA FORSE HO CAPITO MALE IO.
+            }
+        }
+     */
+}
